f16c65cfa701331e112849bbf0fc1fc78f1523e9
Bianca Oliveira <bianca.oliv.bn@gmail.com>
2024-10-22 19:53:48 -0300

    feat: config userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,12 +5,51 @@ import validator from "validator";
 
 //login user
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+
+    
+};
+
+const createToken = (id) => {
+    return jwt.sign({id},)
+}
 
 //register user
 
 const registerUser = async (req, res) => {
-    const {name, password, email}
+  const { name, password, email } = req.body;
+  try {
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+    //hasing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+        name: name,
+        email:email,
+        password:hashedPassword
+    })
+
+    const user = await newUser.save()
+
+
+  } catch (error) {}
 };
 
 export { loginUser, registerUser };
